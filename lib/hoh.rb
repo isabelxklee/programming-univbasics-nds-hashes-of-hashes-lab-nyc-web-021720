87cb10dbf471c6e3704d90d601a4d40babbf09e4
isabel k. lee <hello@isabelklee.co>
2020-02-03 23:22:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -6,7 +6,7 @@
 
 def naming_system
 hash = {
-    label: "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"}},
+    label_1: "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"}},
     label: "Order",
     label: "Family", sub_category: {label: "Genus"},
     label: "Species", sub_category: nil
