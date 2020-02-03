027657d605e3e7ea3088d67cef59c1675a7a3419
isabel k. lee <hello@isabelklee.co>
2020-02-03 23:07:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -6,10 +6,10 @@
 
 def naming_system
 hash = {
-    label: "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"}},
-    label: "Order",
-    label: "Family", sub_category: {label: "Genus"},
-    label: "Species", sub_category: nil
+    PORTION_1 = {label: "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"}}},
+    PORTION_2 = {label: "Order"},
+    PORTION_3 = {label: "Family", sub_category: {label: "Genus"}},
+    PORTION_4 = {label: "Species", sub_category: nil}
 }
 hash
 end
